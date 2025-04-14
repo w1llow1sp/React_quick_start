b2642336bbed56fd982433c23e69d822c26133cd
Mary <mari.front.74@gmail.com>
2025-04-14 18:16:45 +0500

    feat: add functions for editing cells in table

--- src/components/Excel.jsx
+++ src/components/Excel.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import './Excel.css'
 
+function clone(o) {
+    return JSON.parse(JSON.stringify(o));
+}
+
 export class Excel extends React.Component {
     constructor(props) {
         super();
@@ -8,35 +12,53 @@ export class Excel extends React.Component {
             data: props.initialData,
             sortby: null,
             descending: false,
+            edit: null, // { row: index, column: index }
         }
-        this.sort = this.sort.bind(this)
+        this.sort = this.sort.bind(this);
+        this.showEditor = this.showEditor.bind(this);
+        this.save = this.save.bind(this);
     }
-
+    showEditor(e) {
+        this.setState({
+            edit: {
+                row: parseInt(e.target.parentNode.dataset.row, 10),
+                column: e.target.cellIndex,
+            },
+        });
+    }
     sort(e) {
-        const column = e.target.cellIndex
-        const data = clone(this.state.data)
-        const descending = this.state.sortby === column && !this.state.descending
-
-        function clone(o) {
-            return JSON.parse(JSON.stringify(o))
-        }
-
+        const column = e.target.cellIndex;
+        const data = clone(this.state.data);
+        const descending =
+            this.state.sortby === column && !this.state.descending;
         data.sort((a, b) => {
             if (a[column] === b[column]) {
-                return o;
+                return 0;
             }
             return descending
-                ? a[column] < b[column] ? 1 : -1
-                : a[column] > b[column] ? 1 : -1
-
-        })
+                ? a[column] < b[column]
+                    ? 1
+                    : -1
+                : a[column] > b[column]
+                    ? 1
+                    : -1;
+        });
         this.setState({
             data,
             sortby: column,
-            descending
+            descending,
+        });
+    }
+    save(e) {
+        e.preventDefault();
+        const input = e.target.firstChild
+        const data = clone(this.state.data)
+        data[this.state.edit.row][this.state.edit.column] = input.value
+        this.setState({
+            edit:null,
+            data
         })
     }
-
     render() {
         return (
             <table>
@@ -50,12 +72,24 @@ export class Excel extends React.Component {
                     })}
                 </tr>
                 </thead>
-                <tbody>
-                {this.state.data.map((row, indx) => (
-                    <tr key={indx} className={'table_row'}>
-                        {row.map((cell, idx) => (
-                            <td key={idx}>{cell}</td>
-                        ))}
+                <tbody onDoubleClick={this.showEditor}>
+                {this.state.data.map((row, rowidx) => (
+                    <tr key={rowidx} data-row={rowidx}>
+                        {row.map((cell, columnidx) => {
+                            const edit = this.state.edit;
+                            if( edit && edit.row === rowidx && edit.column === columnidx) {
+                                cell = (
+                                    <form onSubmit={this.save}>
+                                        <input type="text" defaultValue={cell} />
+                                    </form>
+
+                                );
+                            }
+                            // TODO — превратить `cell` во вход, если `columnidx`
+                            // и `rowidx` соответствует редактируемому;
+                            // в противном случае просто покажите его как текст
+                            return <td key={columnidx}>{cell}</td>;
+                        })}
                     </tr>
                 ))}
                 </tbody>
