0262de2de14ca0fd72c2197f389c401ae21764ec
Mary <mari.front.74@gmail.com>
2025-04-17 13:42:47 +0500

    feat:add edit

--- src/components/funcComponent/ExcelFuncComponent.jsx
+++ src/components/funcComponent/ExcelFuncComponent.jsx
@@ -12,6 +12,7 @@ export const ExcelFuncComponent = ({headers, initialData}) => {
         column: null,
         descending: false
     })
+    const [edit, setEdit] = useState(null)
 
     function sort(e) {
         const column = e.target.cellIndex;
@@ -34,27 +35,53 @@ export const ExcelFuncComponent = ({headers, initialData}) => {
         setSorting({column, descending})
     }
 
+    function showEditor(e) {
+        setEdit({
+            row: parseInt(e.target.parentNode.dataset.row, 10),
+            column: e.target.cellIndex
+        })
+    }
+
+    function save(e) {
+        e.preventDefault();
+        const input = e.target.firstChild;
+        const dataCopy = clone(data)
+        dataCopy[edit.row][edit.column] = input.value;
+        setEdit(null)
+        setData(dataCopy)
+    }
+
     return (
         <table>
             <thead onClick={sort}>
             <tr>
                 {headers.map((title, idx) => {
-                        if (sorting.column === idx) {
-                            title += sorting.descending ? ' \u2191' : ' \u2193';
-                        }
-                        return <th key={idx}>{title}</th>
+                    if (sorting.column === idx) {
+                        title += sorting.descending ? ' \u2191' : ' \u2193';
                     }
-                )}
+                    return <th key={idx}>{title}</th>;
+                })}
             </tr>
             </thead>
-            <tbody>
-            {data.map((row, idx) =>
-                <tr key={idx}>
-                    {row.map((cell, idx) => (
-                        <td key={idx}>{cell}</td>
-                    ))}
+            <tbody onDoubleClick={showEditor}>
+            {data.map((row, rowidx) => (
+                <tr key={rowidx} data-row={rowidx}>
+                    {row.map((cell, columnidx) => {
+                        if (
+                            edit &&
+                            edit.row === rowidx &&
+                            edit.column === columnidx
+                        ) {
+                            cell = (
+                                <form onSubmit={save}>
+                                    <input type="text" defaultValue={cell}/>
+                                </form>
+                            );
+                        }
+                        return <td key={columnidx}>{cell}</td>;
+                    })}
                 </tr>
-            )}
+            ))}
             </tbody>
         </table>
     )
